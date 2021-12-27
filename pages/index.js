5b5e195fcf95b6f809a0a9f763229439210a970c
reginpv <reginpv@gmail.com>
2021-12-28 02:26:00 +0800

    update index page

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
 
   return (
     <div className="p-5">
+      <h1 className="mb-5 text-lg font-semibold">React, Next.js, Tailwind css, Redux toolkit boilerplate</h1>
       <p><button className="bg-gray-300 p-1" onClick={()=>dispath(setTest(test ? false : true))}>Toggle value</button> {test ? `Test is true` : `Test is false`}</p>
     </div>
   )
