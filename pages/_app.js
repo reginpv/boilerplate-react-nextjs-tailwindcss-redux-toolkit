3bbd00a3e090e39642c9c0584482a78b5445f81a
gin <techdev@xdnainteractive.com>
2022-10-06 11:52:14 +0800

    added sass option, src/scss/app.scss imported in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux'
 import store from '../store'
 
-import 'tailwindcss/tailwind.css'
+import '../src/scss/app.scss'
 
 function MyApp({ Component, pageProps }) {
   return (
